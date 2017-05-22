81fccbea7860080d1d17ea14855fb4966a0393fc
杰 <125549234@qq.com>
2017-05-22 12:29:35 +0000

    Updated database configuration

---
+++ bootstrap/helpers.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathan
+ * Date: 2017/5/20
+ * Time: 下午9:40
+ */
+function get_db_config(){
+    if(getenv('IS_IN_HEROKU')){
+        $url=parse_url(getenv('DATABASE_URL'));
+        return $db_config = [
+            'connetion' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    }else {
+        return $db_config = [
+            'connetion' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
